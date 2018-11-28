b91859555abd2273bbe32e117151a076491256e5
Jonath4nsl <jsl.silentt@gmail.com>
2018-11-28 13:45:34 -0600

    se actualizo la tabla con los datos

--- js/app.js
+++ js/app.js
@@ -6,5 +6,39 @@ btnActualizar.addEventListener('click',Actualizar);
 
 function Actualizar()
     {
-        alert ('actualizar el listado')
-    }
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function()
+      {
+          if(this.readyState == 4 & this.status == 200)
+          {
+          var response = JSON.parse(this.responseText);
+                if(response.status == "ok")
+                {
+                    response.students.forEach(function(student)
+                    {
+                      var row = document.createElement("tr");
+                      var idCell = document.createElement("td");
+                      var firstNameCell = document.createElement("td");
+                      var lastNameCell = document.createElement("td");
+
+                      var idText = document.createTextNode(student.id);
+                      var firstNameText = document.createTextNode(student.first_name);
+                      var lastNameText = document.createTextNode(student.last_name);
+
+                      idCell.appendChild(idText);
+                      firstNameCell.appendChild(firstNameText);
+                      lastNameCell.appendChild(lastNameText);
+
+                      row.appendChild(idCell);
+                      row.appendChild(firstNameCell);
+                      row.appendChild(lastNameCell);
+
+                      document.getElementsByTagName('tbody')[0].appendChild(row);
+
+                    });
+                }
+          }
+      };
+      xhttp.open("GET", "http://nyc.pixan.io/ajax/public/api/students", true);
+      xhttp.send();
+}
