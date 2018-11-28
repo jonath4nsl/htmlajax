4883f99e1290fece169f72706fdad4a6535dfc36
Jonath4nsl <jsl.silentt@gmail.com>
2018-11-28 12:59:04 -0600

    se agrego la funcion del listado JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-alert('hola');
+//obtener la referencia al elemento
+var btnActualizar = document.getElementById('btnActualizar');
+
+//agregar un event listener para cada click
+btnActualizar.addEventListener('click',Actualizar);
+
+function Actualizar()
+    {
+        alert ('actualizar el listado')
+    }
